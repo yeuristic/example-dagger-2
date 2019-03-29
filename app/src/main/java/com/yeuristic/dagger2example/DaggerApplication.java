a113b79bf5f9d94259c78dcb7b0c79d0a64bed1c
yusuf <yusuf@traveloka.com>
2019-03-29 13:47:31 +0700

    Remove feature module dependency form AppEntry

--- app/src/main/java/com/yeuristic/dagger2example/DaggerApplication.java
+++ app/src/main/java/com/yeuristic/dagger2example/DaggerApplication.java
@@ -3,11 +3,13 @@ package com.yeuristic.dagger2example;
 import android.app.Application;
 
 import com.yeuristic.abase.AListener;
+import com.yeuristic.base.ComponentFactory;
 import com.yeuristic.base.Components;
-import com.yeuristic.liba.di.AComponentFactory;
 import com.yeuristic.libaapi.APublicComponent;
 import com.yeuristic.libaapi.APublicDIManager;
 import com.yeuristic.libb.BDIManager;
+
+import java.lang.reflect.InvocationTargetException;
 
 import javax.inject.Inject;
 
@@ -26,7 +28,19 @@ public class DaggerApplication extends Application {
         super.onCreate();
 
         //ComponentFactories
-        Components.MAP.put(APublicComponent.class.getSimpleName(), new AComponentFactory());
+        try {
+            Components.MAP.put(APublicComponent.class.getSimpleName(), (ComponentFactory) Class.forName("com.yeuristic.liba.di.AComponentFactory").getConstructor().newInstance());
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
         mApplicationComponent = DaggerApplicationComponent.builder().aPublicComponent(APublicDIManager.getPublicComponent()).build();
         mApplicationComponent.inject(this);
